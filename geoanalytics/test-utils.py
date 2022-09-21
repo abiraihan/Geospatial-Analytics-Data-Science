b5a1a2320720e8723d1956601bdca61470c8cfac
Abir Raihan <abirraihan.urp@gmail.com>
2022-09-21 21:22:14 +0000

    Added Rtree and STRtree funcionalilty for spatial query

--- geoanalytics/test-utils.py
+++ geoanalytics/test-utils.py
@@ -11,6 +11,7 @@ import re
 import fiona
 import random
 import pyproj
+import warnings
 import numpy as np
 from osgeo import osr
 from tqdm import tqdm
@@ -20,7 +21,10 @@ from shapely.geometry import Polygon, mapping, MultiPolygon, shape
 from shapely.ops import transform
 from sklearn.linear_model import LinearRegression
 import zipfile
-
+import numpy.lib.recfunctions as rfn
+import shapely
+from shapely.errors import ShapelyDeprecationWarning
+warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning)
 # findFile = lambda dirs, ext:[os.path.join(dirs, i) for i in os.listdir(dirs) if os.path.splitext(i)[1] == str('.'+(ext))]
 
 # zipExtract = lambda zipfiles, outDirs : zipfile.ZipFile(zipfiles, 'r').extractall(outDirs)
@@ -94,9 +98,6 @@ from utils import _process_geometry
 # outfilName = f"{outdirs}/{out_palnting_filename}_swath_poly.shp"
 
 # outfiles = createPoly(removes_identical, outfile, x_axis, y_axis, rotation)
-arrays_point, profile_point = geoprocessing.structured_numpy_array(planting)
-
-arrays_poly, profile_poly = geoprocessing.structured_numpy_array(soil_sample)
 
 # data = []
 # for poly in tqdm(list(arrays_poly['geometry'])):
@@ -104,9 +105,113 @@ arrays_poly, profile_poly = geoprocessing.structured_numpy_array(soil_sample)
 #         if point.intersects(poly) == True:
 #             data.append(point)
 
-import rtree
+mzs_path = '/araihan/Reserach_Data/extracted_data/Panola Farming/2022 Soil Sample/Panola Farming_South Panola_SP 12_2022_NO Product_1_poly.shp'
+swath_poly_path = '/araihan/Reserach_Data/extracted_data/Panola Farming/processed_data/planting_point_swath_poly.shp'
+planting = '/araihan/Reserach_Data/extracted_data/Panola Farming/2022 Planting/Panola Farming_South Panola_SP 12_2022_P1718-PRYME_1.shp'
+ecom = '/araihan/Reserach_Data/extracted_data/Panola Farming/ECOM/Panola Farming_South Panola_SP 12_NO Year_TSM_1.shp'
 
-index = rtree.index.Index()
 
-for i, j in enumerate(list(arrays_poly['geometry'])):
-    index.insert(i, j.bounds)
+arrays_point, profile_point = geoprocessing.structured_numpy_array(ecom)
+
+arrays_poly, profile_poly = geoprocessing.structured_numpy_array(mzs_path)
+from shapely.geometry.base import BaseGeometry
+from rtree import index
+from shapely.prepared import prep
+
+import time
+
+
+def query(geom_query, arrays_point, indexes):
+    
+    if isinstance(geom_query, BaseGeometry):
+        query_geom = prep(geom_query)
+        bound = geom_query.bounds
+        tree_idx = [int(i) for i in indexes.intersection(bound)]
+    else:
+        raise TypeError('Not accepted type of geometry')
+    
+    res = []
+    for index_in_tree in tree_idx:
+        if query_geom.contains(arrays_point['geometry'][index_in_tree]):
+            res.append(index_in_tree)
+    tree_idx = res
+    
+    return np.sort(np.array(tree_idx, dtype=np.intp))
+
+def query_bulk(query_geom, tree_geom):
+    
+    tree_indexes = index.Index()
+    
+    for i, j in np.ndenumerate(arrays_point):
+        tree_indexes.insert(i[0], j['geometry'].bounds)
+        
+    tree_index = []
+    input_geometry_index = []
+    
+    for i, geo in enumerate(query_geom['geometry']):
+        res = query(geo, tree_geom, tree_indexes)
+        tree_index.extend(res)
+        input_geometry_index.extend([i] * len(res))
+    return np.vstack([input_geometry_index, tree_index])
+
+start_obj = time.time()
+# dt = query_bulk(arrays_poly, arrays_point)
+
+print("Time to execute Rtree method " + str(time.time() - start_obj))
+
+
+
+# import pygeos
+
+# geom_point = [pygeos.from_shapely(i) for i in arrays_point['geometry']]
+# geom_poly = [pygeos.from_shapely(i) for i in arrays_poly['geometry']]
+
+# tree = pygeos.STRtree(pygeos.points(np.arange(10), np.arange(10)))
+
+# mean_list = [j for i, j in all_mean.items()]
+# a1 = np.array(mean_list, dtype=[('mean', np.float64)])
+
+# arrays = rfn.merge_arrays([arrays_poly, a1], flatten = True, usemask = False)
+
+# from shapely.strtree import STRtree
+# start_obji = time.time()
+# tree = STRtree(list(arrays_point['geometry']))
+
+# index_by_id = dict((id(pt), i) for i, pt in enumerate(list(arrays_point['geometry'])))
+
+# tree_indexi = []
+# input_geometry_indexi = []
+# point_indices = 0  
+# for i, query_geom in enumerate(arrays_poly):
+#     prep_geom = prep(query_geom['geometry'])
+#     selected_point = [(index_by_id[id(pt)], pt.wkt) for pt in tree.query(query_geom['geometry']) if pt.within(query_geom['geometry'])]
+#     indices = [i[0]for i in selected_point]
+#     tree_indexi.extend(np.sort(np.array(indices, dtype = np.intp)))
+#     input_geometry_indexi.extend([i] * len(indices))
+#     # point_indices[id(query_geom['geometry'])] = indices
+#     # point_indices+=len(indices)
+#     # geoVals = np.array([arrays_point[['Rt_Apd_Ct_', 'Year___Yea']][i] for i in indices])
+#     # mean_r4 = geoVals['Rt_Apd_Ct_'].mean()
+#     # mean_r3 = geoVals['Year___Yea'].mean()
+#     # print('Rate Apd value {} and Year value {} for {} number of points'.format(mean_r4, mean_r3, point_indices))
+# query_sbulk = np.vstack([input_geometry_indexi, tree_indexi])
+# print("Time to execute shapely STRtree method " + str(time.time() - start_obji))
+
+
+# print(query_bulk)
+# print(query_sbulk)
+class predicate_type:
+    
+    predicates = {'within', 'contains', 'intersects'} | set([None])
+    
+    @classmethod
+    def assign_predicate(cls, predicate):
+        
+        cls._predicate = predicate
+        if cls._predicate in cls.predicates:
+            return cls._predicate
+        else:
+            return cls.predicates
+
+f = getattr(predicate_type, 'assign_predicate')('intersect')
+print(f)
