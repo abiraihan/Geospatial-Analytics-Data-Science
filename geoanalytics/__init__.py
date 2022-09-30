a09962a3fd9a695851c910176236fbe104377737
Abir Raihan <abirraihan.urp@gmail.com>
2022-09-30 16:47:56 +0000

    - Updated

--- geoanalytics/__init__.py
+++ geoanalytics/__init__.py
@@ -1,2 +1,15 @@
-from core import geoprocessing
-from utils import _process_geometry
+# from base.base import geoprocessing
+# from base.spatialquery.spatialquery import (
+#     SpatialQuery,
+#     GeosQuery,
+#     RtreeQuery,
+#     ComputeArray
+#     )
+# from core import geoprocessing
+# from spatialquery import (
+#     SpatialQuery,
+#     GeosQuery,
+#     RtreeQuery,
+#     ComputeArray
+#     )
+# import base
