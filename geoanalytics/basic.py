0ba6c4c4846863c25c52a7e37cf50f72a3174ad9
Abir Raihan <abirraihan.urp@gmail.com>
2022-10-09 04:17:08 +0000

    geolearn & createshapes added

--- geoanalytics/basic.py
+++ geoanalytics/basic.py
@@ -107,75 +107,111 @@ harvest = '/araihan/Reserach_Data/extracted_data/Panola Farming/2022 Harvest/Pan
 
 
 import collections
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
 
 queryGeom = cores.remove_identical(polyPath, xycoords = True)
 
-harvesting = cores.remove_identical(harvest, xycoords = True)
+# harvesting = cores.remove_identical(harvest, xycoords = True)
 
-pointdata = gpr.structured_numpy_array(ecom)
+# pointdata = gpr.structured_numpy_array(ecom)
 
-plantpoint = gpr.structured_numpy_array(planting)
+# plantpoint = gpr.structured_numpy_array(planting)
 
 unary_poly = shapely.ops.unary_union(queryGeom['geometry'])
 
 arrays = gridBounds(unary_poly, 4326)
-join = gpr.SpatialJoin(arrays[0], queryGeom, 'intersects', *['Soil_pH__1', 'P_lb_ac_', 'K_lb_ac_', 'Soil_CEC_m', 'Soil_OM___'])
-njoin = gpr.SpatialJoin(join, harvesting, 'intersects', *['Yld_Vol_Dr', 'Time'])
-snjoin = gpr.SpatialJoin(njoin, pointdata, 'intersects', *['R4_mS_m_', 'rWTC___', 'D2I_m_'])
-pnjoin = gpr.SpatialJoin(snjoin, plantpoint, 'intersects', *['Rt_Apd_Ct_'])
+
+
+def getColumnLen(gridData:list, sourceCrs:int):
+    projection = lambda geog, proj : pyproj.Transformer.from_crs(
+        crs_from=pyproj.CRS.from_user_input(geog),
+        crs_to=pyproj.CRS.from_user_input(proj),
+        always_xy=True
+        ).transform
+    getbnds = shapely.ops.unary_union(gridData)
+    projected_bound = shapely.ops.transform(
+        projection(4326, 3857),
+        getbnds)
+    
+    minx, miny, maxx, maxy = projected_bound.bounds
+    
+    return len(np.linspace(minx, maxx, int(abs(minx-maxx)/10))) - 1
+
+
+
+
+
+# join = gpr.SpatialJoin(arrays[0], queryGeom, 'intersects', *['Soil_pH__1', 'P_lb_ac_', 'K_lb_ac_', 'Soil_CEC_m', 'Soil_OM___'])
+# njoin = gpr.SpatialJoin(join, harvesting, 'intersects', *['Yld_Vol_Dr', 'Time'])
+# snjoin = gpr.SpatialJoin(njoin, pointdata, 'intersects', *['R4_mS_m_', 'rWTC___', 'D2I_m_'])
+# pnjoin = gpr.SpatialJoin(snjoin, plantpoint, 'intersects', *['Rt_Apd_Ct_'])
 
 # griData = arrays[0]
 
 # cols, rows = arrays[1]
 
+# f8 = {i: j for i, j in pnjoin.dtype.descr if np.dtype(j).kind in ['i', 'f']}
+# idx = {i:list(f8.keys()).index(i) for i, j in f8.items()}
+# final_nparray = pnjoin[list(idx.keys())]
 # join = gpr.SpatialJoin(arrays[0], queryGeom, 'intersects')
 
-# print(join.dtype.names)
-# az = gpd.GeoSeries(data=join['geometry'], crs = 'EPSG:4326')
-# az = gpd.GeoDataFrame(data = join[['R4_m___mean', 'rWTC__mean', 'D2I___mean']], geometry = az)
-# az.plot(column = 'R4_m___mean', figsize = (5,5),linewidth = 1, cmap = 'tab20', edgecolor = 'k')
 
-# A = np.flipud(np.reshape(join['D2I___mean'], (cols, -1)).T)
-# B = np.flipud(np.reshape(join['rWTC__mean'], (cols, -1)).T)
-# C = np.flipud(np.reshape(join['R4_m___mean'], (cols, -1)).T)
-# D = np.flipud(np.reshape(njoin['Yld_o_r_mean'], (cols, -1)).T)
+# uxi = {i:(i, j) for i, j in pnjoin.dtype.descr if np.dtype(j).kind in ['U', 'M']}
+# uidx = {j:list(uxi.keys()).index(i) for i, j in uxi.items()}
+# final_array = pnjoin[[i[0] for i in uidx.keys()]]
+# stack_uarray = np.stack([np.flipud(np.reshape(final_array[i], (cols, -1)).T) for i in final_array.dtype.names])
 
-# array_dict = {
-#     'D2I___mean' : 0,
-#     'rWTC__mean' : 1,
-#     'R4_m___mean' : 2,
-#     'Yld_o_r_mean' : 3
-#     }
+# stack_array = np.stack([np.flipud(np.reshape(final_nparray[i], (cols, -1)).T) for i in final_nparray.dtype.names])
 
-# arr = np.stack([A, B, C, D])
+# def KNC(multiArray, index):
+    
+#     nrow, ncol = multiArray[index].shape
+#     frow, fcol = np.where(np.isfinite(multiArray[index]))
+    
+#     train_X, train_y = [[frow[i], fcol[i]] for i in range(len(frow))], multiArray[index][frow, fcol]
+    
+#     knn = KNeighborsClassifier(n_neighbors= 5, weights='distance', algorithm='ball_tree', p=2)
+#     knn.fit(train_X, train_y)
+    
+#     r2 = knn.score(train_X, train_y)
+#     print("R2- {}".format(r2))
+#     X_pred = [[r, c] for r in range(int(nrow)) for c in range(int(ncol))]
+    
+#     y_pred = knn.predict(X_pred)
+#     karray = np.zeros((nrow, ncol))
+#     i=0
+#     for r in range(int(nrow)):
+#         for c in range(int(ncol)):
+#             karray[r, c] = y_pred[i]
+#             i += 1
+#     return karray
 
-def KNeighborArray(multiArray, index):
+# def KNR(multiArray, index):
     
-    nrow, ncol = multiArray[index].shape
-    frow, fcol = np.where(np.isfinite(multiArray[index]))
+#     nrow, ncol = multiArray[index].shape
+#     frow, fcol = np.where(np.isfinite(multiArray[index]))
     
-    train_X, train_y = [[frow[i], fcol[i]] for i in range(len(frow))], multiArray[index][frow, fcol]
+#     train_X, train_y = [[frow[i], fcol[i]] for i in range(len(frow))], multiArray[index][frow, fcol]
     
-    knn = KNeighborsRegressor(n_neighbors= 5, weights='distance', algorithm='ball_tree', p=2)
-    knn.fit(train_X, train_y)
+#     knn = KNeighborsRegressor(n_neighbors= 5, weights='distance', algorithm='ball_tree', p=2)
+#     knn.fit(train_X, train_y)
     
-    r2 = knn.score(train_X, train_y)
-    print("R2- {}".format(r2))
-    X_pred = [[r, c] for r in range(int(nrow)) for c in range(int(ncol))]
+#     r2 = knn.score(train_X, train_y)
+#     print("R2- {}".format(r2))
+#     X_pred = [[r, c] for r in range(int(nrow)) for c in range(int(ncol))]
     
-    y_pred = knn.predict(X_pred)
-    karray = np.zeros((nrow, ncol))
-    i=0
-    for r in range(int(nrow)):
-        for c in range(int(ncol)):
-            karray[r, c] = y_pred[i]
-            i += 1
-    return karray
+#     y_pred = knn.predict(X_pred)
+#     karray = np.zeros((nrow, ncol))
+#     i=0
+#     for r in range(int(nrow)):
+#         for c in range(int(ncol)):
+#             karray[r, c] = y_pred[i]
+#             i += 1
+#     return karray
 
 # data = griData
-# for name, index in array_dict.items():
-#     vals = np.fliplr(KNeighborArray(arr, index).T).reshape([arr[index].size, 1]).flatten()
+# for name, index in idx.items():
+#     vals = np.fliplr(KNR(stack_array, index).T).reshape([stack_array[index].size, 1]).flatten()
 #     st_array = np.array([vals], dtype = [(name, np.float64)])
 #     data = recfunctions.merge_arrays(
 #         [data, st_array],
@@ -185,8 +221,19 @@ def KNeighborArray(multiArray, index):
 #         )
 #     print(name)
 
-# d2i = KNeighborArray(arr, array_dict['D2I___mean'])
-# print(B)
+# datas = griData
+# for name, index in uidx.items():
+#     vals = np.fliplr(KNC(stack_uarray, index).T).reshape([stack_uarray[index].size, 1]).flatten()
+#     st_array = np.array([vals], dtype = [name])
+#     datas = recfunctions.merge_arrays(
+#         [datas, st_array],
+#         fill_value = np.nan,
+#         flatten=True,
+#         usemask=False
+#         )
+#     print(name[0])
+
+
 # import matplotlib.pyplot as plt
 # plt.imshow(B)
 
---
+++ geoanalytics/geolearn/geolaern.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  9 03:29:03 2022
+
+@author: araihan
+"""
+
